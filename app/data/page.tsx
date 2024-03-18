483720122d9c9bc4094d7a6a8af9f39e498f111d
Kartik Gothwal <kartikgothwal28@gmail.com>
2024-03-19 01:48:04 +0530

    adding the data

---
+++ app/data/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { ReactElement, useEffect, useState } from "react";
+
+const Page = (): ReactElement => {
+  const [fetchData, SetFetchData] = useState<any>();
+  const getDataValues = async () => {
+  const response = await fetch("https://dummyjson.com/products");
+  const data = await response.json();
+  SetFetchData(data.products);
+  };
+  useEffect(() => {
+    getDataValues();
+  }, []);
+  return (
+    <div className="px-5">
+      <ul>
+        {fetchData &&
+          fetchData?.map(
+            (item: { id: number; title: string; description: string }) => (
+              <li className="my-[2rem] border p-4" key={item.id}>
+                <p>title:{item.title}</p>
+                <p>description:{item.description}</p>
+              </li>
+            )
+          )}
+      </ul>
+    </div>
+  );
+};
+
+export default Page;
---
+++ public/assets/images/index.jsx
@@ -0,0 +1,2 @@
+import dish1 from "./dish-1.jpg";
+export { dish1 };
