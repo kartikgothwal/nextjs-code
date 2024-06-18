8defd102fd78cad9d0b3f8ce089d24d4cdba9820
Kartik Gothwal <kartikgothwal28@gmail.com>
2024-06-18 19:29:25 +0530

    dd

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>{children}</header>
+        <header>{children}g</header>
       </body>
     </html>
   );
